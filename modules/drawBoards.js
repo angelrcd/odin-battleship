d949f99b1cc0c878a688e68d943e16c2b6653c56
Angel Romero <mrangelrcd@gmail.com>
2023-08-13 22:56:58 +0200

    Add modal window to place player ships visually

--- modules/drawBoards.js
+++ modules/drawBoards.js
@@ -1,4 +1,4 @@
-const gameboards = document.querySelectorAll('.gameboards-container .gameboard');
+const gameboards = document.querySelectorAll('.gameboard');
 
 gameboards.forEach((gameboard) => {
   for (let row = 1; row <= 10; row++) {
@@ -15,28 +15,55 @@ gameboards.forEach((gameboard) => {
   }
 });
 
-// Highlight coordinates when hovering
+// Highlight coordinates when hovering enemy board
 const enemySquares = document.querySelectorAll('#gameboard-2 .square');
-const columnLegendContainer = document.querySelector('#gameboard-2 .col-legend-container');
-const rowLegendContainer = document.querySelector('#gameboard-2 .row-legend-container');
+const columnLegendContainerEnemy = document.querySelector('#gameboard-2 .col-legend-container');
+const rowLegendContainerEnemy = document.querySelector('#gameboard-2 .row-legend-container');
 
 enemySquares.forEach((enemySquare) => {
   const coordinates = JSON.parse(enemySquare.getAttribute('data-cord'));
 
   enemySquare.addEventListener('mouseover', () => {
-    const colLegend = columnLegendContainer.querySelector(`[data-col="${coordinates.col}"]`);
-    const rowLegend = rowLegendContainer.querySelector(`[data-row="${coordinates.row}"]`);
+    const colLegend = columnLegendContainerEnemy.querySelector(`[data-col="${coordinates.col}"]`);
+    const rowLegend = rowLegendContainerEnemy.querySelector(`[data-row="${coordinates.row}"]`);
 
     colLegend.classList.add('legend-hover');
     rowLegend.classList.add('legend-hover');
   });
 
   enemySquare.addEventListener('mouseout', () => {
-    columnLegendContainer
+    columnLegendContainerEnemy
       .querySelectorAll('.col-legend')
       .forEach((legendCell) => legendCell.classList.remove('legend-hover'));
 
-    rowLegendContainer
+    rowLegendContainerEnemy
+      .querySelectorAll('.row-legend')
+      .forEach((legendCell) => legendCell.classList.remove('legend-hover'));
+  });
+});
+
+// Highlight coordinates when hovering place ship board
+const placeShipSquares = document.querySelectorAll('#gameboard-place-ships .square');
+const columnLegendContainerPlace = document.querySelector('#gameboard-place-ships .col-legend-container');
+const rowLegendContainerPlace = document.querySelector('#gameboard-place-ships .row-legend-container');
+
+placeShipSquares.forEach((placeShipSquare) => {
+  const coordinates = JSON.parse(placeShipSquare.getAttribute('data-cord'));
+
+  placeShipSquare.addEventListener('mouseover', () => {
+    const colLegend = columnLegendContainerPlace.querySelector(`[data-col="${coordinates.col}"]`);
+    const rowLegend = rowLegendContainerPlace.querySelector(`[data-row="${coordinates.row}"]`);
+
+    colLegend.classList.add('legend-hover');
+    rowLegend.classList.add('legend-hover');
+  });
+
+  placeShipSquare.addEventListener('mouseout', () => {
+    columnLegendContainerPlace
+      .querySelectorAll('.col-legend')
+      .forEach((legendCell) => legendCell.classList.remove('legend-hover'));
+
+    rowLegendContainerPlace
       .querySelectorAll('.row-legend')
       .forEach((legendCell) => legendCell.classList.remove('legend-hover'));
   });
