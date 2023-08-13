500e660d3fc4762b7743ea56cf2768bd7575ac00
Angel Romero <mrangelrcd@gmail.com>
2023-08-13 20:24:36 +0200

    Highligh legend when hover square

--- modules/drawBoards.js
+++ modules/drawBoards.js
@@ -14,3 +14,30 @@ gameboards.forEach((gameboard) => {
     }
   }
 });
+
+// Highlight coordinates when hovering
+const enemySquares = document.querySelectorAll('#gameboard-2 .square');
+const columnLegendContainer = document.querySelector('#gameboard-2 .col-legend-container');
+const rowLegendContainer = document.querySelector('#gameboard-2 .row-legend-container');
+
+enemySquares.forEach((enemySquare) => {
+  const coordinates = JSON.parse(enemySquare.getAttribute('data-cord'));
+
+  enemySquare.addEventListener('mouseover', () => {
+    const colLegend = columnLegendContainer.querySelector(`[data-col="${coordinates.col}"]`);
+    const rowLegend = rowLegendContainer.querySelector(`[data-row="${coordinates.row}"]`);
+
+    colLegend.classList.add('legend-hover');
+    rowLegend.classList.add('legend-hover');
+  });
+
+  enemySquare.addEventListener('mouseout', () => {
+    columnLegendContainer
+      .querySelectorAll('.col-legend')
+      .forEach((legendCell) => legendCell.classList.remove('legend-hover'));
+
+    rowLegendContainer
+      .querySelectorAll('.row-legend')
+      .forEach((legendCell) => legendCell.classList.remove('legend-hover'));
+  });
+});
