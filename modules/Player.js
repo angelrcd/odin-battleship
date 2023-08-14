0fb90ba414e62316d8a7b5a3f91cfede715321f0
Angel Romero <mrangelrcd@gmail.com>
2023-08-14 14:26:27 +0200

    Add function to player to place ships randomly

--- modules/Player.js
+++ modules/Player.js
@@ -1,7 +1,33 @@
-export default function Player(gameboard) {
-  this.gameboard = gameboard;
+import Gameboard from './Gameboard';
+const SHIPS_LENGTHS = [5, 4, 3, 3, 2, 2, 1, 1];
+
+export default function Player() {
+  this.gameboard = new Gameboard();
 
   this.attack = function (playerToAttack, col, row) {
     playerToAttack.gameboard.receiveAttack(col, row);
+  };
+
+  // 5 4 3 3 2 2 1 1
+  this.placeShipsRandom = function () {
+    while (this.gameboard.shipList.length < 8) {
+      try {
+        this.placeShipRandom(SHIPS_LENGTHS[this.gameboard.shipList.length]);
+      } catch (e) {}
+    }
+  };
+
+  this.placeShipRandom = function (shipLength) {
+    const orientation = Math.random() < 0.5 ? 'horizontal' : 'vertical';
+    const coordinates = getRandomCoordinates();
+
+    this.gameboard.placeShip(coordinates.col, coordinates.row, shipLength, orientation);
+  };
+}
+
+function getRandomCoordinates() {
+  return {
+    col: Math.floor(Math.random() * 10) + 1,
+    row: Math.floor(Math.random() * 10) + 1,
   };
 }
---
+++ test/Player.test.js
@@ -0,0 +1,26 @@
+import { beforeAll, beforeEach, describe, expect, test } from 'vitest';
+import Player from '../modules/Player';
+
+let player;
+
+beforeEach(() => {
+  player = new Player();
+});
+
+describe('Place ships random', () => {
+  test('Creates 8 ships', () => {
+    expect(player.gameboard.shipList).toHaveLength(0);
+    player.placeShipsRandom();
+    expect(player.gameboard.shipList).toHaveLength(8);
+  });
+
+  test('Ships placed are of the correct size', () => {
+    const SHIPS_LENGTHS = [5, 4, 3, 3, 2, 2, 1, 1];
+    player.placeShipsRandom();
+    const playerShipList = player.gameboard.shipList;
+
+    for (let [index, lengthValue] of SHIPS_LENGTHS.entries()) {
+      expect(playerShipList[index].ship.length).toBe(lengthValue);
+    }
+  });
+});
