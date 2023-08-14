51ee78d6ddea00c5a8bb2ac5b8efd8c51fa88c46
Angel Romero <mrangelrcd@gmail.com>
2023-08-14 15:02:05 +0200

    Squares adjacent to sunk ships reveal visually

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import Player from './modules/Player';
 import Gameboard from './modules/Gameboard';
 import Ship from './modules/Ship';
-import updatePlayerShipsDisplay from './modules/playerShipsDisplay';
+import updatePlayerDisplay from './modules/updateDisplay';
 
 const placeShipsModal = document.querySelector('#place-ships-modal');
 const gameboard1Element = document.querySelector('#gameboard-1');
@@ -9,34 +9,28 @@ const gameboard2Element = document.querySelector('#gameboard-2');
 const enemySquares = gameboard2Element.querySelectorAll('.square');
 // placeShipsModal.showModal();
 
-const player1 = new Player(new Gameboard());
+const player1 = new Player();
+player1.placeShipsRandom();
 
-player1.gameboard.placeShip(1, 1, 4, 'horizontal');
-player1.gameboard.placeShip(2, 7, 2, 'horizontal');
-player1.gameboard.placeShip(5, 3, 3, 'vertical');
-player1.gameboard.placeShip(10, 1, 5, 'vertical');
-updatePlayerShipsDisplay(player1.gameboard.shipList, gameboard1Element, false);
+const player2 = new Player();
+player2.placeShipsRandom();
 
-const player2 = new Player(new Gameboard());
-
-player2.gameboard.placeShip(1, 1, 4, 'horizontal');
-player2.gameboard.placeShip(2, 7, 2, 'horizontal');
-player2.gameboard.placeShip(5, 3, 3, 'vertical');
-player2.gameboard.placeShip(10, 1, 5, 'vertical');
-
-updatePlayerShipsDisplay(player1.gameboard.shipList, gameboard1Element, false);
-updatePlayerShipsDisplay(player2.gameboard.shipList, gameboard2Element, true);
+updatePlayerDisplay(player1, gameboard1Element, false);
+updatePlayerDisplay(player2, gameboard2Element, true);
 
 enemySquares.forEach((enemySquare) => {
   enemySquare.addEventListener('click', () => {
     const coordinates = JSON.parse(enemySquare.getAttribute('data-cord'));
-    enemySquare.classList.add('hit');
     try {
       player1.attack(player2, coordinates.col, coordinates.row);
-      updatePlayerShipsDisplay(player1.gameboard.shipList, gameboard1Element, false);
-      updatePlayerShipsDisplay(player2.gameboard.shipList, gameboard2Element, true);
+      updateDisplay();
     } catch (e) {
       console.log(e);
     }
   });
 });
+
+function updateDisplay() {
+  updatePlayerDisplay(player1, gameboard1Element, false);
+  updatePlayerDisplay(player2, gameboard2Element, true);
+}
--- modules/updateDisplay.js
+++ modules/updateDisplay.js
@@ -1,4 +1,15 @@
-export default function updatePlayerShipsDisplay(playerShipList, gameBoardElement, shipsHidden) {
+export default function updatePlayerDisplay(player, gameBoardElement, areShipsHidden) {
+  for (let square of player.gameboard.board) {
+    const squareElement = gameBoardElement.children[2].children[square.col - 1 + (square.row - 1) * 10];
+    if (square.isHit) {
+      squareElement.classList.add('hit');
+    }
+  }
+
+  updatePlayerShipsDisplay(player.gameboard.shipList, gameBoardElement, areShipsHidden);
+}
+
+function updatePlayerShipsDisplay(playerShipList, gameBoardElement, shipsHidden) {
   for (let shipData of playerShipList) {
     for (let shipCell of shipData.cells) {
       const shipCellCol = shipCell.col;
